21b31147e259b86dfea8df3283c80eb6765bb640
CHunter <nekto@must-die.ru>
2021-06-29 23:33:41 +0300

    Update 02_hw.py

--- Module2/home_work/02_hw.py
+++ Module2/home_work/02_hw.py
@@ -7,4 +7,13 @@
 # коров, корова или коровы
 # Например, 1 корова, 2 коровы, 5 коров, 125 коров.
 
-# TODO: your code here
+print("Enter natural number")
+number_of_cow = int(input("Количество коров: "))
+variant_name = [2, 0, 1, 1, 1, 2]
+cow_name = ['корова', 'коровы', 'коров']
+if 4 < number_of_cow % 100 < 20:
+    print(number_of_cow, cow_name[2])
+elif number_of_cow % 10 < 5:
+    print(number_of_cow, cow_name[variant_name[number_of_cow % 10]])
+else:
+    print(number_of_cow, cow_name[variant_name[5]])
